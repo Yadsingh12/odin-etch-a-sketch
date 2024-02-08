56ad44183d9a5af3737ff6e2f0638a5e48ebe513
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-02-08 16:19:29 +0530

    Add button to change number of grid squares.
    To accomodate the changed number of grid squares, we needed to change some css too.
    
    Along with that, we changed the parent node's name to container to avoid confusion.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const parent = document.querySelector('#parent');
+const container = document.querySelector('#container');
 for(let i=0; i<16; i++){
     const row = document.createElement('div');
     row.className = 'row';
@@ -7,7 +7,7 @@ for(let i=0; i<16; i++){
         child.className = 'child';
         row.appendChild(child);
     }
-    parent.appendChild(row);
+    container.appendChild(row);
 }
 
 const square = document.querySelectorAll(".child");
@@ -15,3 +15,28 @@ square.forEach(element => {
     element.addEventListener('mouseover',()=> {element.classList.add('hovered')});
     element.addEventListener('mouseleave',()=> {element.classList.remove('hovered')});
 });
+
+const body = document.querySelector('body');
+const btn = document.createElement('button');
+btn.textContent = "CHANGE";
+body.prepend(btn);
+
+btn.addEventListener('click',()=> {
+    const gridSize = Number(prompt("number of squares per side for the new grid"));
+    if(isNaN(gridSize)) return;
+    else {
+        while(container.hasChildNodes()){
+            container.removeChild(container.firstChild);
+        }
+        for(let i=0; i<gridSize; i++){
+            const row = document.createElement('div');
+            row.className = 'row';
+            for(let j=0; j<gridSize; j++){
+                const child = document.createElement('div');
+                child.className = 'child';
+                row.appendChild(child);
+            }
+            container.appendChild(row);
+        }
+    }
+})
