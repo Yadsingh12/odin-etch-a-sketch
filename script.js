99924703553bbd331a009747029c0cea6cfa3dfa
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-02-08 16:32:51 +0530

    Add check condition for gridSize
    If the user cancel the prompt, the prompt() returns null, for which we need to check.
    If gridSize is null then we need to return from the function.

--- script.js
+++ script.js
@@ -24,24 +24,25 @@ body.prepend(btn);
 btn.addEventListener('click', () => {
     const gridSize = Number(prompt("number of squares per side for the new grid"));
     if (isNaN(gridSize)) return;
-    else {
-        while (container.hasChildNodes()) {
-            container.removeChild(container.firstChild);
-        }
-        for (let i = 0; i < gridSize; i++) {
-            const row = document.createElement('div');
-            row.className = 'row';
-            for (let j = 0; j < gridSize; j++) {
-                const child = document.createElement('div');
-                child.className = 'child';
-                row.appendChild(child);
-            }
-            container.appendChild(row);
-            const square = document.querySelectorAll(".child");
-            square.forEach(element => {
-                element.addEventListener('mouseover', () => { element.classList.add('hovered') });
-                element.addEventListener('mouseleave', () => { element.classList.remove('hovered') });
-            });
-        }
+    if (gridSize === null) return;
+
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
     }
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.className = 'row';
+        for (let j = 0; j < gridSize; j++) {
+            const child = document.createElement('div');
+            child.className = 'child';
+            row.appendChild(child);
+        }
+        container.appendChild(row);
+        const square = document.querySelectorAll(".child");
+        square.forEach(element => {
+            element.addEventListener('mouseover', () => { element.classList.add('hovered') });
+            element.addEventListener('mouseleave', () => { element.classList.remove('hovered') });
+        });
+    }
+
 })
