439c84d1c8cba9964eebd89db26cac19774e351a
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-02-08 16:26:30 +0530

    Add color changing of grid squares to button event listener
    The grid square's color was not changing while hovering over them after their number is changed.
    Therefore, I copied the eventlistener code for hovering and pasted it to button's event listener.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('#container');
-for(let i=0; i<16; i++){
+for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.className = 'row';
-    for(let j=0; j<16; j++){
+    for (let j = 0; j < 16; j++) {
         const child = document.createElement('div');
         child.className = 'child';
         row.appendChild(child);
@@ -12,8 +12,8 @@ for(let i=0; i<16; i++){
 
 const square = document.querySelectorAll(".child");
 square.forEach(element => {
-    element.addEventListener('mouseover',()=> {element.classList.add('hovered')});
-    element.addEventListener('mouseleave',()=> {element.classList.remove('hovered')});
+    element.addEventListener('mouseover', () => { element.classList.add('hovered') });
+    element.addEventListener('mouseleave', () => { element.classList.remove('hovered') });
 });
 
 const body = document.querySelector('body');
@@ -21,22 +21,27 @@ const btn = document.createElement('button');
 btn.textContent = "CHANGE";
 body.prepend(btn);
 
-btn.addEventListener('click',()=> {
+btn.addEventListener('click', () => {
     const gridSize = Number(prompt("number of squares per side for the new grid"));
-    if(isNaN(gridSize)) return;
+    if (isNaN(gridSize)) return;
     else {
-        while(container.hasChildNodes()){
+        while (container.hasChildNodes()) {
             container.removeChild(container.firstChild);
         }
-        for(let i=0; i<gridSize; i++){
+        for (let i = 0; i < gridSize; i++) {
             const row = document.createElement('div');
             row.className = 'row';
-            for(let j=0; j<gridSize; j++){
+            for (let j = 0; j < gridSize; j++) {
                 const child = document.createElement('div');
                 child.className = 'child';
                 row.appendChild(child);
             }
             container.appendChild(row);
+            const square = document.querySelectorAll(".child");
+            square.forEach(element => {
+                element.addEventListener('mouseover', () => { element.classList.add('hovered') });
+                element.addEventListener('mouseleave', () => { element.classList.remove('hovered') });
+            });
         }
     }
 })
